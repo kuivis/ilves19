09c223bd1c3df9f6cbc0bc4419a732c843f0a4be
Sami Kuivasaari <kuivasami@Samis-MacBook-Pro.local>
2018-04-07 15:42:10 +0300

    ew

--- ilves19-plugin/ilves19-plugin.php
+++ ilves19-plugin/ilves19-plugin.php
@@ -7,69 +7,167 @@ Description: Ilves19 sivuston modaukset tulevat tätä kautta
 
 
 
-// Register and load the widget
-function wpb_load_widget() {
-    register_widget( 'wpb_widget' );
-}
-add_action( 'widgets_init', 'wpb_load_widget' );
+class WP_Widget_Ilves extends WP_Widget {
 
-// Creating the widget
-class wpb_widget extends WP_Widget {
 
-function __construct() {
-parent::__construct(
+	public function __construct() {
+		$widget_ops = array(
+			'classname' => 'widget_categories_ilves',
+			'description' => __( 'A list or dropdown of categories. With exceptions.' ),
+			'customize_selective_refresh' => true,
+		);
+		parent::__construct( 'categories', __( 'Categories' ), $widget_ops );
+	}
 
-// Base ID of your widget
-'wpb_widget',
+	/**
+	 * Outputs the content for the current Categories widget instance.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @staticvar bool $first_dropdown
+	 *
+	 * @param array $args     Display arguments including 'before_title', 'after_title',
+	 *                        'before_widget', and 'after_widget'.
+	 * @param array $instance Settings for the current Categories widget instance.
+	 */
+	public function widget( $args, $instance ) {
+		static $first_dropdown = true;
 
-// Widget name will appear in UI
-__('WPBeginner Widget', 'wpb_widget_domain'),
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Categories' );
 
-// Widget description
-array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'wpb_widget_domain' ), )
-);
-}
+		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
+		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
 
-// Creating widget front-end
+		$c = ! empty( $instance['count'] ) ? '1' : '0';
+		$h = ! empty( $instance['hierarchical'] ) ? '1' : '0';
+		$d = ! empty( $instance['dropdown'] ) ? '1' : '0';
 
-public function widget( $args, $instance ) {
-$title = apply_filters( 'widget_title', $instance['title'] );
+		echo $args['before_widget'];
 
-// before and after widget arguments are defined by themes
-echo $args['before_widget'];
-if ( ! empty( $title ) )
-echo $args['before_title'] . $title . $args['after_title'];
+		if ( $title ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
 
-// This is where you run the code and display the output
-echo __( 'Hello, Worldäerjwäejr!', 'wpb_widget_domain' );
-echo $args['after_widget'];
-}
+		$cat_args = array(
+			'orderby'      => 'name',
+			'show_count'   => $c,
+			'hierarchical' => $h,
+      'exclude_tree' => '1,5',
+		);
 
-// Widget Backend
-public function form( $instance ) {
-if ( isset( $instance[ 'title' ] ) ) {
-$title = $instance[ 'title' ];
-}
-else {
-$title = __( 'New title', 'wpb_widget_domain' );
-}
-// Widget admin form
-?>
-<p>
-<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-</p>
+		if ( $d ) {
+			echo sprintf( '<form action="%s" method="get">', esc_url( home_url() ) );
+			$dropdown_id = ( $first_dropdown ) ? 'cat' : "{$this->id_base}-dropdown-{$this->number}";
+			$first_dropdown = false;
+
+			echo '<label class="screen-reader-text" for="' . esc_attr( $dropdown_id ) . '">' . $title . '</label>';
+
+			$cat_args['show_option_none'] = __( 'Select Category' );
+			$cat_args['id'] = $dropdown_id;
+
+			/**
+			 * Filters the arguments for the Categories widget drop-down.
+			 *
+			 * @since 2.8.0
+			 * @since 4.9.0 Added the `$instance` parameter.
+			 *
+			 * @see wp_dropdown_categories()
+			 *
+			 * @param array $cat_args An array of Categories widget drop-down arguments.
+			 * @param array $instance Array of settings for the current widget.
+			 */
+			wp_dropdown_categories( apply_filters( 'widget_categories_dropdown_args', $cat_args, $instance ) );
+
+			echo '</form>';
+			?>
+
+<script type='text/javascript'>
+/* <![CDATA[ */
+(function() {
+	var dropdown = document.getElementById( "<?php echo esc_js( $dropdown_id ); ?>" );
+	function onCatChange() {
+		if ( dropdown.options[ dropdown.selectedIndex ].value > 0 ) {
+			dropdown.parentNode.submit();
+		}
+	}
+	dropdown.onchange = onCatChange;
+})();
+/* ]]> */
+</script>
+
 <?php
+		} else {
+?>
+		<ul>
+<?php
+		$cat_args['title_li'] = '';
+
+		/**
+		 * Filters the arguments for the Categories widget.
+		 *
+		 * @since 2.8.0
+		 * @since 4.9.0 Added the `$instance` parameter.
+		 *
+		 * @param array $cat_args An array of Categories widget options.
+		 * @param array $instance Array of settings for the current widget.
+		 */
+		wp_list_categories( apply_filters( 'widget_categories_args', $cat_args, $instance ) );
+?>
+		</ul>
+<?php
+		}
+
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Handles updating settings for the current Categories widget instance.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param array $new_instance New settings for this instance as input by the user via
+	 *                            WP_Widget::form().
+	 * @param array $old_instance Old settings for this instance.
+	 * @return array Updated settings to save.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+		$instance['count'] = !empty($new_instance['count']) ? 1 : 0;
+		$instance['hierarchical'] = !empty($new_instance['hierarchical']) ? 1 : 0;
+		$instance['dropdown'] = !empty($new_instance['dropdown']) ? 1 : 0;
+
+		return $instance;
+	}
+
+	/**
+	 * Outputs the settings form for the Categories widget.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param array $instance Current settings.
+	 */
+	public function form( $instance ) {
+		//Defaults
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '') );
+		$title = sanitize_text_field( $instance['title'] );
+		$count = isset($instance['count']) ? (bool) $instance['count'] :false;
+		$hierarchical = isset( $instance['hierarchical'] ) ? (bool) $instance['hierarchical'] : false;
+		$dropdown = isset( $instance['dropdown'] ) ? (bool) $instance['dropdown'] : false;
+		?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /></p>
+
+		<p><input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('dropdown'); ?>" name="<?php echo $this->get_field_name('dropdown'); ?>"<?php checked( $dropdown ); ?> />
+		<label for="<?php echo $this->get_field_id('dropdown'); ?>"><?php _e( 'Display as dropdown' ); ?></label><br />
+
+		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>"<?php checked( $count ); ?> />
+		<label for="<?php echo $this->get_field_id('count'); ?>"><?php _e( 'Show post counts' ); ?></label><br />
+
+		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('hierarchical'); ?>" name="<?php echo $this->get_field_name('hierarchical'); ?>"<?php checked( $hierarchical ); ?> />
+		<label for="<?php echo $this->get_field_id('hierarchical'); ?>"><?php _e( 'Show hierarchy' ); ?></label></p>
+		<?php
+	}
 }
-
-// Updating widget replacing old instances with new
-public function update( $new_instance, $old_instance ) {
-$instance = array();
-$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-return $instance;
-}
-} // Class wpb_widget ends here
-
-
 /* Stop Adding Functions Below this Line */
 ?>
